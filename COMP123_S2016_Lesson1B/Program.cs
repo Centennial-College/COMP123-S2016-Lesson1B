e7e59d48d34c9f0d7aa8ec690f5cf6d1b6f53d1c
Kb-Ma <kbma-kevin@gmail.com>
2016-05-18 13:21:26 -0400

    initial commit

---
+++ COMP123_S2016_Lesson1B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+    Author: Kevin Ma
+    Date Created: May 18, 2016
+    Date Modified: May 18, 2016
+    Student ID: 300867968
+
+    Description: This program demonstrates GitHub and version control best practices.
+    We also explore Unit Testing
+
+    Version: 0.1 - Initial Version
+ */
+
+namespace COMP123_S2016_Lesson1B
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
